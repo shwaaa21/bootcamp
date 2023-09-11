c8a9627d09a1d008b555522654d98b024e599906
josh <gun19333@gmail.com>
2023-09-11 11:19:59 +1000

    Svelte Feedback App - tutorial followed @traversymedia

---
+++ svelte-feedback-app/components/FeedbackList.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { FeedbackStore } from "../stores";
+    import { fade, scale } from "svelte/transition";
+    import FeedbackItem from "./FeedbackItem.svelte";
+    let feedback = [];
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+    <div in:scale out:fade={{ duration: 500 }}>
+        <FeedbackItem item={fb} />
+    </div>
+{/each}
